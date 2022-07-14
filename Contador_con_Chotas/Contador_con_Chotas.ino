09711cc278171fe1fa59c4aa7c2845d3b567039c
BloodLike <98510365+BloodLike@users.noreply.github.com>
2022-07-14 00:18:37 -0500

    tercer_commit

--- Contador_con_Chotas/Contador_con_Chotas.ino
+++ Contador_con_Chotas/Contador_con_Chotas.ino
@@ -1,9 +1,15 @@
 int Display[7] = {2,3,4,5,6,7,8};                                               //PINES DEL DISPLAY 
 int unidades = 9, decenas = 10, centenas = 11, millar = 12;   //PINES DE DIGITOS
+int a=0,b=0,c=0,d=0;
 
 void setup() {
-
-
+  
+for(int i=0; i<7; i++){                                                                 //FOR DE 1 AL 6
+pinMode(Display[i], OUTPUT);                                                 //DECLARAR TODO EL CONTENIDO DEL ARRAY COMO SALIDA
+}
+for (int i=9; i<13; i++){                                                               //FOR 9 AL 13
+pinMode(i,OUTPUT);                                                                //PINES 9 AL 12 COMO SALIDA
+}
 }
 
 void loop() {
