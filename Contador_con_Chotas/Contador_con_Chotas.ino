522c612b2671b38f45a9037eb6e77f185694e1bf
BloodLike <98510365+BloodLike@users.noreply.github.com>
2022-07-14 00:14:00 -0500

    segundo_commit

---
+++ Contador_con_Chotas/Contador_con_Chotas.ino
@@ -0,0 +1,12 @@
+int Display[7] = {2,3,4,5,6,7,8};                                               //PINES DEL DISPLAY 
+int unidades = 9, decenas = 10, centenas = 11, millar = 12;   //PINES DE DIGITOS
+
+void setup() {
+
+
+}
+
+void loop() {
+
+
+}
